b332934f06071e4a3002e8858c1bc9e7bff53671
Juan Bogado <juanjobogado13@gmail.com>
2023-04-20 23:44:07 -0300

    Landing animation ready

--- my-app/src/components/LandingPage/LandingPage.jsx
+++ my-app/src/components/LandingPage/LandingPage.jsx
@@ -1,13 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./LandingPage.module.css";
+import logo from "../images and gifs/starWarsLogoNegro.png";
 
 export default function LandingPage() {
+  const [isStopped, setIsStopped] = useState(false);
+
+  useEffect(() => {
+    const container = document.querySelector(".container");
+
+    const handleScroll = () => {
+      const containerTop = container.offsetTop;
+      const containerHeight = container.offsetHeight;
+      const windowTop = window.scrollY;
+      const windowHeight = window.innerHeight;
+
+      if (
+        containerTop + containerHeight / 2 > windowTop + windowHeight / 2 &&
+        !isStopped
+      ) {
+        setIsStopped(true);
+        container.classList.add(styles.stop);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [isStopped]);
+
   return (
     <main className={styles.container}>
       <header>
-        <h2 className={styles.h2Landing}>Star Wars Challenge</h2>
-        <p className={styles.pLanding}> May the force be with you on this amazing adventure! Discover everything about Star Wars movies and their characters on our web application, where you can explore SWAPI data and filter by gender and eye color. Be a true Jedi in navigating our page and enjoy an incredibly responsive experience!</p>
+        <img src={logo} />
+        <p className={styles.pLanding}>
+          May the force be with you on this amazing adventure! Discover
+          everything about Star Wars movies and their characters on our web
+          application, where you can explore SWAPI data and filter by gender
+          and eye color. Be a true Jedi in navigating our page and enjoy an
+          incredibly responsive experience!
+        </p>
       </header>
       <div className={styles.btn}>
         <Link to="/home">
@@ -16,4 +50,4 @@ export default function LandingPage() {
       </div>
     </main>
   );
-};
+}
